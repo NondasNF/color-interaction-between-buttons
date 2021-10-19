54c62f0c248a7fa71b5af288d6b6eb7ce2730db8
Epaminondas Noronha Feitosa <54701024+NondasNF@users.noreply.github.com>
2021-10-19 14:26:09 -0300

    Add main code

---
+++ index.js
@@ -0,0 +1,68 @@
+var currentButtonActive;
+var allButtons;
+var colors;
+var currentButtonSizeActiveIndex = 2;
+var currentButtonSizeActive;
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    currentButtonActive = document.querySelector("#b1");
+    allButtons = document.querySelectorAll(".b");
+    colors = ["#0000FF", "#008000", "#D2691E", "#FF0000"];
+    currentButtonActive.style.backgroundColor = colors[0];
+    currentButtonActive.style.color = "#ffffff";
+
+    currentButtonSizeActive = document.querySelector("#bs3")
+    currentButtonSizeActive.style.color = "#ffffff";
+    currentButtonSizeActive.style.backgroundColor = colors[0];
+
+}, false);
+
+function getOthersButtons(buttonClicked, allColorButtons) {
+    var newButtonObject = {
+        otherButtons:[],
+        buttonClickedIndex:null
+    };
+    allColorButtons.forEach(function (button, indexButton, arrayButtons) {
+        if (buttonClicked.id != button.id) {
+            newButtonObject.otherButtons.push(button);
+        }else{
+            newButtonObject.buttonClickedIndex = indexButton;
+        }
+    })
+    return newButtonObject;
+}
+function changeSizeButtonColor(color){
+    let allSizeButtons = document.querySelectorAll(".bs");
+    let currentSizeButton = allSizeButtons[currentButtonSizeActiveIndex];
+    currentSizeButton.style.backgroundColor = color;
+}
+function changeColor(buttonClicked) {
+    let allColorButtons = document.querySelectorAll(".b");
+    let buttonsObject = getOthersButtons(buttonClicked, allColorButtons);
+    buttonsObject.otherButtons.forEach(function(otherButton){
+        otherButton.style.backgroundColor = "";
+        otherButton.style.color = "#000000";
+    });
+    buttonClicked.style.backgroundColor = colors[buttonsObject.buttonClickedIndex];
+    buttonClicked.style.color = "#ffffff";
+    changeSizeButtonColor(colors[buttonsObject.buttonClickedIndex]);
+}
+
+function changeSize(buttonClicked){
+    let allSizeButtons = document.querySelectorAll(".bs");
+    let sizeButtonsObject = getOthersButtons(buttonClicked, allSizeButtons);
+
+    let currentColor = allSizeButtons[currentButtonSizeActiveIndex];
+    let currentBackgroundColor = allSizeButtons[currentButtonSizeActiveIndex];
+
+    buttonClicked.style.color = currentColor.style.color;
+    buttonClicked.style.backgroundColor = currentBackgroundColor.style.backgroundColor;
+    buttonClicked.style.color = "#ffffff";
+
+    sizeButtonsObject.otherButtons.forEach(function(otherButton){
+        otherButton.style.backgroundColor = "";
+        otherButton.style.color = "#000000";
+    });
+    currentButtonSizeActiveIndex = sizeButtonsObject.buttonClickedIndex;
+}
